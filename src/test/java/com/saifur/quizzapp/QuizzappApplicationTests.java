6c9a5328a3ce778ab0df3dafa11d374cb928e21f
saifur2233 <saifurnstuiit223344@gmail.com>
2024-01-13 11:29:53 +0600

    Issue -1: Initial Configuration Setup

---
+++ src/test/java/com/saifur/quizzapp/QuizzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saifur.quizzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
