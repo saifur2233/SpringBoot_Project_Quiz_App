a3db176a2a20b14b5b9c7e2680e392e785cbbb71
saifur2233 <saifurnstuiit223344@gmail.com>
2024-01-13 12:00:12 +0600

    Issue-3.1: Question add controller and service added

--- src/main/java/com/saifur/quizzapp/controller/QuestionController.java
+++ src/main/java/com/saifur/quizzapp/controller/QuestionController.java
@@ -1,15 +1,20 @@
 package com.saifur.quizzapp.controller;
 
 import com.saifur.quizzapp.model.Question;
+import com.saifur.quizzapp.service.QuestionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
 
     @PostMapping("add")
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
--- src/main/java/com/saifur/quizzapp/service/QuestionService.java
+++ src/main/java/com/saifur/quizzapp/service/QuestionService.java
@@ -1,12 +1,17 @@
 package com.saifur.quizzapp.service;
 
+import com.saifur.quizzapp.dao.QuestionDao;
 import com.saifur.quizzapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
 
     public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
